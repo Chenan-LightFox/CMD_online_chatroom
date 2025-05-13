a221773d3aefdcda177ed0c58fccdc0c4738041e
bl3x3 <byangshuxiangb@outlook.com>
2025-05-14 01:47:47 +0800

    setup MatchEngine

--- CMDoc_server/header_file/MatchEngine.h
+++ CMDoc_server/header_file/MatchEngine.h
@@ -1,9 +1,32 @@
 #pragma once
 
-#include <vector>
 #include "Chatroom.h"
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 class MatchEngine {
-public:
-    ChatRoom* matchUserToRoom(User* user);
+  public:
+    ChatRoom *matchUserToRoom(User *user);
 };
+
+class Tokenizer {
+  private:
+    std::set<std::string> dict;
+
+  public:
+    std::vector<std::string> fmmTokenizer(const std::string &str);
+};
+
+class FeatureExtractor {
+  private:
+  public:
+    static std::map<std::string, std::vector<double>>
+    extractFeatures(const std::vector<std::string> &chats,
+                    const std::vector<std::string> &keywords);
+};
+
+class Normalizer {};
+
+class Similarity {};
